aa6a006df5bb6fe48a056606251596c60950b737
Antonio Dell'Elce <antonio.dellelce@thomsonreuters.com>
2016-02-04 11:48:14 +0000

    introducing a "pdf context" / cleanups

--- ad.c
+++ ad.c
@@ -1,8 +1,8 @@
 /* 
-   File:    bindump.c
+   File:    pdfdump.c
    Created: 1449 120814
 
-   mini binary dumper
+   mini pdf dumper
 */
 
 // includes
@@ -37,61 +37,67 @@ pushchar(pstate_t *s, unsigned char ch)
   return 0;
 }
 
+typedef struct 
+{
+ // count binary zeros: just one will not print anything, more will print a blank
+ // int   zerocnt = 0;
+ FILE            *fh;           // pdf file handle
+ int              ch;
+ int              cnt = 0;      // number of characters we printed
+ pstate_t         state;
+ unsigned char    ver[10];
+ unsigned char    verclose = 0;
+ unsigned long    gcnt = 0;     // global counter - count all chars read
+ unsigned short   verread = 0;  // version is NOT read = 0 - read = 1
+} pdf_context_t;
+
 // main
 
 int
 main (int argc, char **argv)
 {
- FILE    *fh;
- int      ch;
- int      cnt = 0;     // number of characters we printed
-// int   zerocnt = 0; // count binary zeros: just one will not print anything, more will print a blank
- pstate_t       s;
- unsigned char  ver[10];
- unsigned char  verclose = 0;
- unsigned long  gcnt = 0;    // global counter - count all chars read
- unsigned short verread = 0; // version is NOT read = 0 - read = 1
+ pdf_context_t pdf;
 
 // Initialize stuff
 
- ver[0] = 0;
+ pdf.ver[0] = 0;
  
- fh = fopen(argv[1], "rb");
+ pdf.fh = fopen(argv[1], "rb");
 
- if (fh == NULL)
+ if (pdf.fh == NULL)
  {
    return 1;
  }
 
- while (!feof(fh))
+ while (!feof(pdf.fh))
  {
-  ch = fgetc(fh);
+  pdf.ch = fgetc(pdf.fh);
 
   // read PDF version
-  if (gcnt < (sizeof(ver)-1)) // slightly buggy condition as it is 
+  if (pdf.gcnt < (sizeof(pdf.ver)-1)) // slightly buggy condition as it is 
   {
-    if (verread == 0)
+    if (pdf.verread == 0)
     {
-      if (ch == 0xa || ch == 0xd)
+      if (pdf.ch == 0xa || pdf.ch == 0xd)
       {
-        verread = 1;
-        verclose = ch;
-        printf ("Version: %s\n", &ver[0]);
-        printf ("Version closing char: %02x\n", ch);
+        pdf.verread  = 1;
+        pdf.verclose = ch;
+        printf ("Version: %s\nVersion closing char: %02x\n", &pdf.ver[0], pdf.ch);
       }
       else
       {
-        ver[gcnt] = ch;
-        ver[gcnt+1] = 0;
+        pdf.ver[gcnt]   = pdf.ch;
+        pdf.ver[gcnt+1] = 0;
       }
     }
   }
   
-  gcnt = gcnt + 1;
+  pdf.gcnt = pdf.gcnt + 1;
 
-  if (cnt >= LINESIZE)
+  if (pdf.cnt >= LINESIZE)
   {
-   cnt = 0; printf("\n");
+   pdf.cnt = 0;
+   printf("\n");
   }
 
 /*
@@ -106,7 +112,7 @@ main (int argc, char **argv)
   }
 */
 
-  if (ch < 32)
+  if (pdf.ch < 32)
   {
 /*
    if (ch == '\n')
@@ -114,38 +120,38 @@ main (int argc, char **argv)
     if (cnt != 0) { cnt = 0; printf("\n"); }; continue;
    }
 */
-   if (ch == 8)
+   if (pdf.ch == 8)
    {
-    cnt += 4; printf("    "); continue;
+    pdf.cnt += 4; printf("    "); continue;
    }
 
    // CR
 
-   if (ch == verclose)
+   if (pdf.ch == pdf.verclose)
    {
-    cnt = 0;
+    pdf.cnt = 0;
     printf(" <EOL>\n");
     continue;
    }
 
    //printf(DOT);
-   printf("<%02x>", ch);
+   printf("<%02x>", pdf.ch);
    cnt += 4;
    continue;
   }
 
-  if (ch > 126)
+  if (pdf.ch > 126)
   {
-   printf("<%02x>", ch);
-   cnt += 4;
+   printf("<%02x>", pdf.ch);
+   pdf.cnt += 4;
    continue;
   }
 
-  printf("%c", ch); 
-  cnt += 1;
+  printf("%c", pdf.ch); 
+  pdf.cnt += 1;
  } // while
 
- if (cnt > 0) { printf("\n"); }  
+ if (pdf.cnt > 0) { printf("\n"); }  
 
  return 0;
 }
