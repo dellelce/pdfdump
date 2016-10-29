acae23e9db85ae3699223c33e43734f9b19bbdfc
Antonio Dell'Elce <antouk@dellelce.com>
2016-10-29 04:33:52 +0100

    util

---
+++ util.c
@@ -0,0 +1,22 @@
+/* 
+
+   File:    util.c
+   Created: 291016
+
+*/
+
+// includes
+
+#include <pdf.h>
+
+/*
+   pdf_context_init
+ */
+
+void
+pdf_context_int(pdf_context_t *context)
+{
+
+}
+
+// ** EOF **
